49d5862f636267f161fa20cefc7ec4ed2215b7ab
vibha2 <juhisahu2203@gmail.com>
2023-03-30 18:23:49 +0530

    Functionality added done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+import {useRef} from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const windowWidth = useRef(window.innerWidth);
+  // const windowHeight = useRef(window.innerHeight);
+
+  console.log('width: ', windowWidth.current);
+  // console.log('height: ', windowHeight.current);
+
+  const [width, setWidth] = useState("ok");
+
+  useEffect( ()=> {
+    handleSubmit();
+  });
+
+  const handleSubmit = () => {
+    setWidth(windowWidth.current);
+  }
   return (
-    <div>
-      Hello
+    <div className="wrapper">
+      <form onChange={handleSubmit}>
+        <label>
+          Window Size:
+          <input type="number" value={width} readOnly={true} ></input>
+        </label>
+      </form>
     </div>
   );
 }
